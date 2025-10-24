61cddb4e4afdafd47e7cc0f5cc28914cd12f213b
cong <3135055939@qq.com>
2025-10-24 22:37:56 +0800

    解耦 用list表示产品

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from TFWR import *
 
+xType = [Entities.Grass, Entities.Carrot, Entities.Carrot, Entities.Carrot, Entities.Bush, Entities.Bush]
+
+def fit(types):
+    if types == Entities.Carrot and get_ground_type() != Grounds.Soil:
+        till()
+    elif types != Entities.Carrot and get_ground_type() == Grounds.Soil:
+        till()
+
 while True:
-	for i in range(get_world_size()):
-		move(East)
-		for j in range(get_world_size()):
-			move(North)
-			if can_harvest():
-				harvest()
-				if i == 0:
-					plant(Entities.Bush)
-				elif i == 1:
-					plant(Entities.Carrot)
-			if get_water() <= 0.8:
-				use_item(Items.Water)
+    while get_pos_x() > 0:
+        move(West)
+    while get_pos_y() > 0:
+        move(South)
+
+    for i in range(get_world_size()):
+        move(East)
+        for j in range(get_world_size()):
+            move(North)
+            x = get_pos_x()
+            y = get_pos_y()
+            if can_harvest():
+                harvest()
+                type = xType[x]
+                fit(type)
+                plant(type)
+            if get_water() <= 0.8:
+                use_item(Items.Water)
